468ce3b9a0644bdd480645fa212bcc84c31f89b8
wangwwno1 <wangwwno1@gmail.com>
2022-09-07 14:24:59 +0800

    Extract std var for clarity

--- src/modules/sensors/vehicle_gps_position/gps_validation.cpp
+++ src/modules/sensors/vehicle_gps_position/gps_validation.cpp
@@ -157,8 +157,11 @@ namespace sensors
                 _last_pos_error(2) = ref_pos_board(2) + (gps_position.alt * 1.e-3f - _gps_alt_ref);
 
                 // test ratio = residual / std_error = residual / sqrt(vars)
-                _pos_validator.validate(_last_pos_error.edivide(_last_pos_vars.sqrt()));
-                _vel_validator.validate(_last_vel_error.edivide(_last_vel_vars.sqrt()));
+                // Error offset will be applied internally in the AbsErrorTimeWindow detector
+                const Vector3f pos_std_var = _last_pos_vars.sqrt();
+                const Vector3f vel_std_var = _last_vel_vars.sqrt();
+                _pos_validator.validate(_last_pos_error.edivide(pos_std_var));
+                _vel_validator.validate(_last_vel_error.edivide(vel_std_var));
 
                 PublishSensorStatus();
                 PublishErrorStatus();
