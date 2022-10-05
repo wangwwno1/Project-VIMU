6cd66eca20f00df5e576f78bed32a007df643a96
wangwwno1 <wangwwno1@gmail.com>
2022-10-05 15:18:02 +0800

    Bug Fix: wrong param reference

--- src/modules/sensors/vehicle_imu/VehicleIMU.cpp
+++ src/modules/sensors/vehicle_imu/VehicleIMU.cpp
@@ -58,8 +58,8 @@ VehicleIMU::VehicleIMU(int instance, uint8_t accel_index, uint8_t gyro_index, co
     _param_iv_gyr_ema_h(_gyro_validator_params.ema_params.control_limit),
     _param_iv_gyr_alpha(_gyro_validator_params.ema_params.alpha),
     _param_iv_gyr_ema_cap(_gyro_validator_params.ema_params.cap),
-    _param_iv_acc_csum_h(_accel_validator_params.control_limit),
-    _param_iv_acc_mshift(_accel_validator_params.mean_shift),
+    _param_iv_acc_csum_h(_accel_validator_params.cusum_params.control_limit),
+    _param_iv_acc_mshift(_accel_validator_params.cusum_params.mean_shift),
     _param_iv_acc_ema_h(_accel_validator_params.ema_params.control_limit),
     _param_iv_acc_alpha(_accel_validator_params.ema_params.alpha),
     _param_iv_acc_ema_cap(_accel_validator_params.ema_params.cap)
