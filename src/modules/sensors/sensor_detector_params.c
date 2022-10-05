1e7f4aa81bef1dd399740b8f2aaa71b70163613e
wangwwno1 <wangwwno1@gmail.com>
2022-10-05 12:51:32 +0800

    VIMU: Add EWMA param for GPS POS

--- src/modules/sensors/sensor_detector_params.c
+++ src/modules/sensors/sensor_detector_params.c
@@ -55,6 +55,40 @@ PARAM_DEFINE_FLOAT(IV_GPS_P_CSUM_H, 0.f);
 PARAM_DEFINE_FLOAT(IV_GPS_P_MSHIFT, 1.f);
 
 /**
+ * Control limit for Exponential Moving Average (EMA) validation of the gps position innovation, expressed in standard deviation.
+ *
+ * The formula is EMA(T) = min(max(val, -CAP), +CAP) * Alpha + (1-Alpha) * EMA(T-1)
+ * Set zero to inhibit validation.
+ *
+ * @group Innovation Validator
+ * @min 0.0
+ * @decimal 4
+ */
+PARAM_DEFINE_FLOAT(IV_GPS_P_EMA_H, 0.f);
+
+/**
+ * Alpha for Exponential Moving Average (EMA) validation of the gps position innovation.
+ *
+ * @group Innovation Validator
+ * @min 0.0001
+ * @max 1.0
+ * @decimal 4
+ */
+PARAM_DEFINE_FLOAT(IV_GPS_P_ALPHA, 1.f);
+
+/**
+ * Clip value for Exponential Moving Average (EMA) validation of the gps position innovation, expressed in standard deviation.
+ *
+ * This constrain the maximum value that will input into EMA detector.
+ * Set zero to inhibit clip, for inhibit validation please refer to the control limit description.
+ *
+ * @group Innovation Validator
+ * @min 0.0
+ * @decimal 2
+ */
+PARAM_DEFINE_FLOAT(IV_GPS_P_EMA_CAP, 0.f);
+
+/**
  * Control limit for CuScore validation of the gps velocity innovation, expressed in standard deviations, set zero to inhibit validation.
  *
  * @group Innovation Validator
