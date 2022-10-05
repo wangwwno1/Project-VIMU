c5826bde3b197c08f07ed2c02eb9568589296631
wangwwno1 <wangwwno1@gmail.com>
2022-10-05 15:42:05 +0800

    Bug Fix: cannot find _params

--- src/lib/fault_detector/SquareErrorTimeWindowVector.hpp
+++ src/lib/fault_detector/SquareErrorTimeWindowVector.hpp
@@ -13,33 +13,34 @@ namespace fault_detector {
     template<typename Type, size_t N = 1>
     class SquareErrorTimeWindowVector : public AbsErrorTimeWindowVector<Type, N> {
     public:
+        using ParentClass = AbsErrorTimeWindowVector<Type, N>;
         using ParamStruct = TimeWindowParams<Type>;
         using typename DetectorVector<Type, N>::VectorN;
 
 
         SquareErrorTimeWindowVector() = delete;
 
-        SquareErrorTimeWindowVector(ParamStruct *params) : _param(params) { reset(); }
+        SquareErrorTimeWindowVector(ParamStruct *params) : ParentClass(params) { reset(); }
 
         ~SquareErrorTimeWindowVector() = default;
 
-        using AbsErrorTimeWindowVector<Type, N>::reset;
+        using ParentClass::reset;
 
         using DetectorVector<Type, N>::validate;
 
         bool validate(const VectorN &innov_ratios) {
             // squared_values = x * abs(x)
             const VectorN squared_values = innov_ratios.emult(innov_ratios.abs());
-            return AbsErrorTimeWindowVector<Type, N>::validate(squared_values);
+            return ParentClass::validate(squared_values);
         }
 
-        using AbsErrorTimeWindowVector<Type, N>::error_sum;
-        using AbsErrorTimeWindowVector<Type, N>::error_offset;
-        using AbsErrorTimeWindowVector<Type, N>::error_offsets;
+        using ParentClass::error_sum;
+        using ParentClass::error_offset;
+        using ParentClass::error_offsets;
 
-        using AbsErrorTimeWindowVector<Type, N>::reset_error_offset;
-        using AbsErrorTimeWindowVector<Type, N>::test_ratio;
-        using AbsErrorTimeWindowVector<Type, N>::test_ratios;
+        using ParentClass::reset_error_offset;
+        using ParentClass::test_ratio;
+        using ParentClass::test_ratios;
     };
 
     using SquareErrorTimeWindowVectorf = SquareErrorTimeWindowVector<float>;
