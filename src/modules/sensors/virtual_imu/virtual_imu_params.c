27405b8a742786e0e8e04c97331f02544a0dd183
wangwwno1 <wangwwno1@gmail.com>
2023-02-17 14:56:34 +0800

    Add Parameter for Non-Linear Model

---
+++ src/modules/sensors/virtual_imu/virtual_imu_params.c
@@ -0,0 +1,19 @@
+//
+// Created by Robert Wang on 2022/7/7.
+//
+
+/**
+ * @min 1000
+ * @max 1000000
+ * @reboot_required true
+ * @unit us
+ */
+PARAM_DEFINE_INT32(IV_IMU_DELAY_US, 200000);
+
+/**
+ * @min 1000
+ * @max 100000
+ * @reboot_required true
+ * @unit us
+ */
+PARAM_DEFINE_INT32(VIMU_PREDICT_US, 20000);
