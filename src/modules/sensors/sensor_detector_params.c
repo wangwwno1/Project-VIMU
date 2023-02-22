1b2167bee7aa47d298110202f2ea49e162b14e42
wangwwno1 <wangwwno1@gmail.com>
2023-02-22 09:51:14 +0800

    Remove unused time window detector parameters

--- src/modules/sensors/sensor_detector_params.c
+++ src/modules/sensors/sensor_detector_params.c
@@ -84,102 +84,6 @@ PARAM_DEFINE_FLOAT(IV_GPS_V_CSUM_H, 0.f);
  */
 PARAM_DEFINE_FLOAT(IV_GPS_V_MSHIFT, 1.f);
 
-
-/**
- * Control limit for Exponential Moving Average (EMA) validation of the gps velocity innovation, expressed in standard deviation.
- *
- * The formula is EMA(T) = min(max(val, -CAP), +CAP) * Alpha + (1-Alpha) * EMA(T-1)
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_GPS_V_EMA_H, 0.f);
-
-/**
- * Alpha for Exponential Moving Average (EMA) validation of the gps velocity innovation.
- *
- * @group Innovation Validator
- * @min 0.0001
- * @max 1.0
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_GPS_V_ALPHA, 1.f);
-
-/**
- * Clip value for Exponential Moving Average (EMA) validation of the gps velocity innovation, expressed in standard deviation.
- *
- * This constrain the maximum value that will input into EMA detector.
- * Set zero to inhibit clip, for inhibit validation please refer to the control limit description.
- *
- * @group Innovation Validator
- * @min 0.0
- * @decimal 2
- */
-PARAM_DEFINE_FLOAT(IV_GPS_V_EMA_CAP, 0.f);
-
-/**
- * Control limit for Time Window (TWIN) validation of the gps position innovation, expressed in standard deviations.
- *
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @reboot_required true
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_GPS_P_TWIN_H, 0.f);
-
-
-/**
- * Minimum samples required before reset gps position TWIN detector.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_GPS_P_RST_CNT, 1);
-
-/**
- * Minimum consecutive normal samples before declare the sensor is normal.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_GPS_P_CD_CNT, 1);
-
-/**
- * Control limit for Time Window (TWIN) validation of the gps velocity innovation, expressed in standard deviations.
- *
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @reboot_required true
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_GPS_V_TWIN_H, 0.f);
-
-/**
- * Minimum samples required before reset gps velocity TWIN detector.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_GPS_V_RST_CNT, 1);
-
-/**
- * Minimum consecutive normal samples before declare the sensor is normal.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_GPS_V_CD_CNT, 1);
-
 /**
  * Control limit for CuScore validation of the barometer height innovation.
  *
@@ -206,36 +110,6 @@ PARAM_DEFINE_FLOAT(IV_BARO_CSUM_H, 0.f);
 PARAM_DEFINE_FLOAT(IV_BARO_MSHIFT, 1.f);
 
 /**
- * Control limit for Time Window (TWIN) validation of the barometer rate innovation, expressed in standard deviations.
- *
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @reboot_required true
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_BARO_TWIN_H, 0.f);
-
-/**
- * Minimum samples required before reset barometer TWIN detector.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_BARO_RST_CNT, 1);
-
-/**
- * Minimum consecutive normal samples before declare the sensor is normal.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_BARO_CD_CNT, 1);
-
-/**
  * Control limit for CuScore validation of magnetometer innovation.
  *
  * Expressed in standard deviations when the respective quadratic ratio is equal to zero, or variances if the ratio is set to one.
@@ -260,36 +134,6 @@ PARAM_DEFINE_FLOAT(IV_MAG_CSUM_H, 0.f);
  * @decimal 4
  */
 PARAM_DEFINE_FLOAT(IV_MAG_MSHIFT, 1.f);
-
-/**
- * Control limit for Time Window (TWIN) validation of the magnetometer rate innovation, expressed in standard deviations.
- *
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @reboot_required true
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_MAG_TWIN_H, 0.f);
-
-/**
- * Minimum samples required before reset magnetometer TWIN detector.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_MAG_RST_CNT, 1);
-
-/**
- * Minimum consecutive normal samples before declare the sensor is normal.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_MAG_CD_CNT, 1);
 
 /**
  * Accelerometer noise for innovation validation, it is equal to the standard deviation of the residual between the virtual and real sensor.
@@ -363,36 +207,6 @@ PARAM_DEFINE_FLOAT(IV_ACC_ALPHA, 1.f);
 PARAM_DEFINE_FLOAT(IV_ACC_EMA_CAP, 0.f);
 
 /**
- * Control limit for Time Window (TWIN) validation for acceleration, expressed in standard deviations.
- *
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @reboot_required true
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_ACC_TWIN_H, 0.f);
-
-/**
- * Minimum samples required before reset accelerometer TWIN detector.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_ACC_RST_CNT, 1);
-
-/**
- * Minimum consecutive normal samples before declare the sensor is normal.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_ACC_CD_CNT, 1);
-
-/**
  * Rate gyro noise for innovation validation, it is equal to the standard deviation of the residual between virtual gyro and real gyro.
  *
  * @group Innovation Validator
@@ -459,33 +273,3 @@ PARAM_DEFINE_FLOAT(IV_GYR_ALPHA, 1.f);
  * @decimal 2
  */
 PARAM_DEFINE_FLOAT(IV_GYR_EMA_CAP, 0.f);
-
-/**
- * Control limit for Time Window (TWIN) validation of the gyro rate innovation, expressed in standard deviations.
- *
- * Set zero to inhibit validation.
- *
- * @group Innovation Validator
- * @min 0.0
- * @reboot_required true
- * @decimal 4
- */
-PARAM_DEFINE_FLOAT(IV_GYR_TWIN_H, 0.f);
-
-/**
- * Minimum samples required before reset gyro TWIN detector.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_GYR_RST_CNT, 1);
-
-/**
- * Minimum consecutive normal samples before declare the sensor is normal.
- *
- * @group Innovation Validator
- * @min 1
- * @reboot_required true
- */
-PARAM_DEFINE_INT32(IV_GYR_CD_CNT, 1);
