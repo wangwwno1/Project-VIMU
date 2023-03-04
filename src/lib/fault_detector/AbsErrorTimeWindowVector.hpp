913e619a6477ab9af8d56b7e2c4004c008894979
wangwwno1 <wangwwno1@gmail.com>
2023-03-04 10:01:32 +0800

    Unify interface & add update_offset API to time window detector

--- src/lib/fault_detector/AbsErrorTimeWindowVector.hpp
+++ src/lib/fault_detector/AbsErrorTimeWindowVector.hpp
@@ -21,7 +21,7 @@ namespace fault_detector {
         ~AbsErrorTimeWindowVector() = default;
 
         void reset() {
-            _sum_absolute_error.setAll(+Type(0.));
+            _error_sum.setAll(+Type(0.));
             reset_error_offset();
 
             _sample_counter = 0;
@@ -47,36 +47,19 @@ namespace fault_detector {
                 // this would create a deadlock after attack is detected (i.e. upper sum > control limit).
                 // Therefore, we modify the original condition and use a larger safe window.
                 if (_sample_counter >= _param->reset_samples) {
-                    if (_is_normal && _normal_sample_counter >= _param->reset_samples) {
+                    if (_update_offset && _is_normal && _normal_sample_counter >= _param->reset_samples) {
                         // Calculate average offset in last window
-                        _error_offset = _error_cusum / _sample_counter;
-                        _error_cusum.setAll(+Type(0.));
+                        _error_offset = _normal_error_cusum / _sample_counter;
+                        _normal_error_cusum.setAll(+Type(0.));
                         _normal_sample_counter = 0;
                     }
-                    _sum_absolute_error.setAll(+Type(0.));
+                    _error_sum.setAll(+Type(0.));
                     _sample_counter = 0;
                 };
 
                 // validate the ratio
                 _is_running = true;
-                const VectorN error = innov_ratios / _param->control_limit;
-
-                _sum_absolute_error += (error - _error_offset).abs();
-                _sum_absolute_error = matrix::constrain(_sum_absolute_error, Type(0.), +Type(1.05));
-                _sample_counter++;
-
-                if (_sum_absolute_error.max() >= +Type(1.0)) {
-                    // Declare faulty, discard all previous normal samples, reset safe counter
-                    _error_cusum.setAll(+Type(0.));
-                    _normal_sample_counter = 0;
-                    _safe_counter = 0;
-                    _is_normal = false;
-                } else {
-                    if (!_is_normal) _safe_counter++;
-                    // Only count error offsets in normal
-                    _error_cusum += error;
-                    _normal_sample_counter++;
-                }
+                update_status(innov_ratios);
 
                 this->_error_mask = (_is_normal) ? ERROR_FLAG_SUM_UCL_EXCEED : ERROR_FLAG_NO_ERROR;
 
@@ -88,32 +71,66 @@ namespace fault_detector {
             }
         }
 
-        const VectorN &error_sum() const { return _sum_absolute_error; }
+        // used for synchronize offset update between detectors
+        bool update_offset() { return _update_offset; }
+        void set_update_offset(bool update_offset) { _update_offset = update_offset; }
 
-        const Type error_offset() const { return _error_offset(0) * _param->control_limit; }
+        const VectorN &error_sum() const { return _error_sum; }
 
-        const VectorN error_offsets() const { return _error_offset * _param->control_limit; }
+        const Type error_offset() const { return _error_offset(0) * detect_threshold(); }
+
+        const VectorN error_offsets() const { return _error_offset * detect_threshold(); }
 
         void reset_error_offset() { _error_offset.setAll(+Type(0.)); }
 
+        const Type detect_threshold() const {
+            return _param->control_limit;
+        }
+
         const Type test_ratio() const {
-            return _is_normal ? _sum_absolute_error.max() : math::max(_sum_absolute_error.max(), Type(+1.0001));
+            return _is_normal ? test_ratio_raw() : math::max(test_ratio_raw(), Type(+1.0001));
+        }
+
+        const Type test_ratio_raw() const {
+            return test_ratios().max();
         }
 
         const VectorN test_ratios() const {
-            return _sum_absolute_error;
+            return _error_sum;
         }
 
-    private:
+    protected:
+        void update_status(const VectorN &innov_ratios) {
+            const VectorN error = innov_ratios / detect_threshold();
+
+            _error_sum += (error - _error_offset).abs();
+            _error_sum = matrix::constrain(_error_sum, Type(0.), +Type(1.05));
+            _sample_counter++;
+
+            if (test_ratio_raw() >= +Type(1.0)) {
+                // Declare faulty, discard all previous normal samples, reset safe counter
+                _normal_error_cusum.setAll(+Type(0.));
+                _normal_sample_counter = 0;
+                _safe_counter = 0;
+                _is_normal = false;
+            } else {
+                if (!_is_normal) _safe_counter++;
+                // Only count error offsets in normal
+                _normal_error_cusum += error;
+                _normal_sample_counter++;
+            }
+        }
+
         ParamStruct *_param;
-        VectorN _sum_absolute_error;
-        VectorN _error_cusum;
+        VectorN _error_sum;
+        VectorN _normal_error_cusum;
         VectorN _error_offset;
         int32_t _sample_counter{0};
         int32_t _normal_sample_counter{0};
         int32_t _safe_counter{0};
         bool _is_normal{true};
         bool _is_running{false};
+        bool _update_offset{true};
     };
 
     using AbsErrorTimeWindowf = AbsErrorTimeWindowVector<float>;
--- src/lib/fault_detector/SquareErrorTimeWindowVector.hpp
+++ src/lib/fault_detector/SquareErrorTimeWindowVector.hpp
@@ -20,7 +20,7 @@ namespace fault_detector {
         ~SquareErrorTimeWindowVector() = default;
 
         void reset() {
-            _sum_squared_error.setAll(+Type(0.));
+            _error_sum.setAll(+Type(0.));
             reset_error_offset();
 
             _sample_counter = 0;
@@ -46,36 +46,19 @@ namespace fault_detector {
                 // this would create a deadlock after attack is detected (i.e. upper sum > control limit).
                 // Therefore, we modify the original condition and use a larger safe window.
                 if (_sample_counter >= _param->reset_samples) {
-                    if (_is_normal && _normal_sample_counter >= _param->reset_samples) {
+                    if (_update_offset && _is_normal && _normal_sample_counter >= _param->reset_samples) {
                         // Calculate average offset in last window
-                        _error_offset = _error_cusum / _sample_counter;
-                        _error_cusum.setAll(+Type(0.));
+                        _error_offset = _normal_error_cusum / _sample_counter;
+                        _normal_error_cusum.setAll(+Type(0.));
                         _normal_sample_counter = 0;
                     }
-                    _sum_squared_error.setAll(+Type(0.));
+                    _error_sum.setAll(+Type(0.));
                     _sample_counter = 0;
                 };
 
                 // validate the ratio
                 _is_running = true;
-                const VectorN relative_error = innov_ratios / sqrt(detect_threshold());
-                const VectorN corrected_error = relative_error - _error_offset;
-
-                _sum_squared_error += corrected_error.emult(corrected_error);
-                _sample_counter++;
-
-                if (test_ratio() >= +Type(1.0)) {
-                    // Declare faulty, discard all previous normal samples, reset safe counter
-                    _error_cusum.setAll(+Type(0.));
-                    _normal_sample_counter = 0;
-                    _safe_counter = 0;
-                    _is_normal = false;
-                } else {
-                    if (!_is_normal) _safe_counter++;
-                    // Only count relative_error offsets in normal
-                    _error_cusum += relative_error;
-                    _normal_sample_counter++;
-                }
+                update_status(innov_ratios);
 
                 this->_error_mask = (_is_normal) ? ERROR_FLAG_SUM_UCL_EXCEED : ERROR_FLAG_NO_ERROR;
 
@@ -87,37 +70,66 @@ namespace fault_detector {
             }
         }
 
-        const VectorN &error_sum() const { return _sum_squared_error; }
+        // used for synchronize offset update between detectors
+        bool update_offset() { return _update_offset; }
+        void set_update_offset(bool update_offset) { _update_offset = update_offset; }
 
-        const Type error_offset() const { return _error_offset(0) * sqrt(detect_threshold()); }
+        const VectorN &error_sum() const { return _error_sum; }
 
-        const VectorN error_offsets() const { return _error_offset * sqrt(detect_threshold()); }
+        const Type error_offset() const { return _error_offset(0) * detect_threshold(); }
+
+        const VectorN error_offsets() const { return _error_offset * detect_threshold(); }
 
         void reset_error_offset() { _error_offset.setAll(+Type(0.)); }
 
         const Type detect_threshold() const {
-            return _param->control_limit / static_cast<Type>(math::max(_param->reset_samples, 1));
+            return sqrt(_param->control_limit / static_cast<Type>(math::max(_param->reset_samples, 1)));
         }
 
         const Type test_ratio() const {
-            const Type test_ratio = test_ratios().max();
-            return _is_normal ? test_ratio : math::max(test_ratio, Type(+1.0001));
+            return _is_normal ? test_ratio_raw() : math::max(test_ratio_raw(), Type(+1.0001));
+        }
+
+        const Type test_ratio_raw() const {
+            return test_ratios().max();
         }
 
         const VectorN test_ratios() const {
-            return _sum_squared_error / static_cast<Type>(math::max(_sample_counter, 1));
+            return _error_sum / static_cast<Type>(math::max(_sample_counter, 1));
         }
 
-    private:
+    protected:
+        void update_status(const VectorN &innov_ratios) {
+            const VectorN relative_error = innov_ratios / detect_threshold();
+            const VectorN corrected_error = relative_error - _error_offset;
+
+            _error_sum += corrected_error.emult(corrected_error);
+            _sample_counter++;
+
+            if (test_ratio_raw() >= +Type(1.0)) {
+                // Declare faulty, discard all previous normal samples, reset safe counter
+                _normal_error_cusum.setAll(+Type(0.));
+                _normal_sample_counter = 0;
+                _safe_counter = 0;
+                _is_normal = false;
+            } else {
+                if (!_is_normal) _safe_counter++;
+                // Only count relative_error offsets in normal
+                _normal_error_cusum += relative_error;
+                _normal_sample_counter++;
+            }
+        }
+
         ParamStruct *_param;
-        VectorN _sum_squared_error;
-        VectorN _error_cusum;
+        VectorN _error_sum;
+        VectorN _normal_error_cusum;
         VectorN _error_offset;
         int32_t _sample_counter{0};
         int32_t _normal_sample_counter{0};
         int32_t _safe_counter{0};
         bool _is_normal{true};
         bool _is_running{false};
+        bool _update_offset{true};
     };
 
     using SquareErrorTimeWindowVectorf = SquareErrorTimeWindowVector<float>;
